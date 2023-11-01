f37f04dce2db7939d0b67bff318ac6352c0cf76a
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2023-10-31 21:01:10 -0300

    [:construction:] Adicionado listagem por CNPJ

--- src/controllers/ClienteController.js
+++ src/controllers/ClienteController.js
@@ -1,3 +1,4 @@
+const ErroBase = require("../errors/ErroBase");
 const database = require("../models");
 
 class ClienteController {
@@ -8,7 +9,25 @@ class ClienteController {
       if(empresasCadastradas.length !== 0) {
         res.status(200).send(empresasCadastradas)
       }
-      next("Nenhuma empresa cadastrada", 404);
+
+      next(new ErroBase("Nenhuma empresa cadastrada!", 404));
+
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async listarPorCnpj(req, res, next) {
+    const cnpjDaEmpresa = req.nr_cnpj;
+    const empresaEncontrada = await database.T_LGC_CLIENTE.findOne({ where : {nr_cnpj: cnpjDaEmpresa}});
+
+    try {
+      if(empresaEncontrada) {
+        res.status(200).send(empresaEncontrada);
+      };
+
+      next(new ErroBase("Empresa não encontrada.", 404));
+      
     } catch (err) {
       next(err);
     }
@@ -29,8 +48,15 @@ class ClienteController {
     };
 
     try {
+      const existeCnpj = await database.T_LGC_CLIENTE.findOne({ where : {nr_cnpj: novaEmpresa.nr_cnpj}});
+
+      if(existeCnpj) {
+        next(new ErroBase("CNPJ de empresa existente", 401));
+      }
+      
       const empresaCriada = await database.T_LGC_CLIENTE.create(novaEmpresa);
       res.status(200).send(empresaCriada);      
+
     } catch (err) {
       next(err);
     }
